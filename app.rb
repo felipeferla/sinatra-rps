62b1c416990ee0233cc084153a8aacd1593d5636
felipeferla <164975883+felipeferla@users.noreply.github.com>
2024-04-09 20:54:17 +0000

    hey

--- app.rb
+++ app.rb
@@ -7,3 +7,51 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "We tied!"
+  elsif
+    @comp_move == "paper"
+    @outcome = "We lost!"
+  else
+    @outcome = "We won!"
+  end
+
+ erb(:rock)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "paper"
+    @outcome = "We tied!"
+  elsif
+    @comp_move == "scissors"
+    @outcome = "We lost!"
+  else
+    @outcome = "We won!"
+  end
+
+ erb(:paper)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "scissors"
+    @outcome = "We tied!"
+  elsif
+    @comp_move == "rock"
+    @outcome = "We lost!"
+  else
+    @outcome = "We won!"
+  end
+
+ erb(:scissors)
+end
